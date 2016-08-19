0e34fea00e79b1bdc386dbb3dd7ee2c99f349411
Chris Shaw <c.mshaw@yahoo.co.uk>
2016-08-19 11:29:25 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='arcgis2geojson',
+    author="chris48s",
+    license="MIT",
+    url="https://github.com/chris48s/arcgis2geojson/",
+    py_modules=['arcgis2geojson'],
+)
