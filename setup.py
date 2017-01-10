ed60455959b4d1c9022deafc2264e4612a7b4c76
chris48s <c.mshaw@yahoo.co.uk>
2017-01-10 20:03:45 +0000

    1.0.0 release

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(
     name='arcgis2geojson',
+    version='1.0.0',
     author="chris48s",
     license="MIT",
     url="https://github.com/chris48s/arcgis2geojson/",
