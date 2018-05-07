b11dcfe77163b489ad9045654bb50aa05a195375
chris48s <chris.shaw480@gmail.com>
2018-05-07 13:43:22 +0100

    use markdown for long description

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-pandoc --from=markdown --to=rst --output=README.rst README.md
-python setup.py sdist
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 def _get_description():
     try:
-        path = os.path.join(os.path.dirname(__file__), 'README.rst')
+        path = os.path.join(os.path.dirname(__file__), 'README.md')
         with io.open(path, encoding='utf-8') as f:
             return f.read()
     except IOError:
@@ -14,6 +14,7 @@ setup(
     name='arcgis2geojson',
     description='A Python library for converting ArcGIS JSON to GeoJSON',
     long_description=_get_description(),
+    long_description_content_type="text/markdown",
     version='1.2.0',
     author="chris48s",
     license="MIT",
